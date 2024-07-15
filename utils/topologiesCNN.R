0a1a8deed7de0acf9512073bf1789f91c845ca9d
oscarmirones <mironeso@unican.es>
2024-07-15 13:27:06 +0200

    adecuate notebook to the new paper in JGR:Machine Learning and computation. Still some changes need to be made

--- utils/topologiesCNN.R
+++ utils/topologiesCNN.R
@@ -1,38 +1,8 @@
 ## function cnn_model ---------
 cnn_model <- function(topology, input_shape, output_shape, kernel_size, neurons, prior = NULL, latent_dim = NULL, weight = NULL) {
   
-  # cnn-single-site -------------------------------------------------------
-  if (topology == "cnn-single-site-gaussian") {
-    inputs <- layer_input(shape = input_shape)
-    l1 = layer_conv_2d(inputs,filters = 50, kernel_size = kernel_size, activation = "relu", padding = "valid")
-    l2 = layer_conv_2d(l1, filters = 25, kernel_size = kernel_size, activation = "relu", padding = "valid")
-    l3 = layer_conv_2d(l2, filters = 10, kernel_size = kernel_size, activation = "relu", padding = "valid") 
-    l4 = layer_flatten(l3)
-    l5 = layer_dense(l4, units = neurons[1], activation = "relu") 
-    l6 = layer_dense(l5, units = neurons[2], activation = "relu") 
-    outputs = layer_dense(l6, units = output_shape, activation = "linear") 
-    model <- keras_model(inputs = inputs, outputs = outputs) 
-    ## -------------------    
-    return(model) 
-  }
-  
-  # cnn-multi-site -------------------------------------------------------
-  if (topology == "cnn-multi-site") {
-    inputs <- layer_input(shape = input_shape)
-    l1 = layer_conv_2d(inputs,filters = 50, kernel_size = kernel_size, activation = "relu", padding = "valid")
-    l2 = layer_conv_2d(l1, filters = 25, kernel_size = kernel_size, activation = "relu", padding = "valid")
-    l3 = layer_conv_2d(l2, filters = 10, kernel_size = kernel_size, activation = "relu", padding = "valid") 
-    l4 = layer_flatten(l3)
-    l5 = layer_dense(l4, units = neurons[1], activation = "relu") 
-    l6 = layer_dense(l5, units = neurons[2], activation = "relu") 
-    outputs = layer_dense(l6, units = output_shape, activation = "linear") 
-    model <- keras_model(inputs = inputs, outputs = outputs) 
-    ## -------------------    
-    return(model) 
-  }
-  
-  # cnn-multi-site-gaussian -------------------------------------------------------
-  if (topology == "cnn-multi-site-gaussian") {
+  # cnn-gaussian -------------------------------------------------------
+  if (topology == "cnn-gaussian") {
     inputs <- layer_input(shape = input_shape)
     l1 = layer_conv_2d(inputs,filters = 50, kernel_size = kernel_size, activation = "relu", padding = "valid")
     l2 = layer_conv_2d(l1, filters = 25, kernel_size = kernel_size, activation = "relu", padding = "valid")
@@ -47,9 +17,10 @@ cnn_model <- function(topology, input_shape, output_shape, kernel_size, neurons,
     ## -------------------    
     return(model) 
   }
-
-  # cnn-multi-site-multi-gaussian -------------------------------------------------------
-  if (topology == "cnn-multi-site-multi-gaussian") {
+  
+  
+  # cnn-multi-gaussian -------------------------------------------------------
+  if (topology == "cnn-multi-gaussian") {
     model <- keras_model_sequential() %>%
       layer_conv_2d(input_shape = input_shape, filters = 50, kernel_size = kernel_size, activation = "relu", padding = "valid") %>% 
       layer_conv_2d(filters = 25, kernel_size = kernel_size, activation = "relu", padding = "valid") %>% 
@@ -59,30 +30,8 @@ cnn_model <- function(topology, input_shape, output_shape, kernel_size, neurons,
       layer_dense(units = neurons[2], activation = "relu")  %>% 
       layer_dense(units = params_size_multivariate_normal_tri_l(event_size = output_shape)) %>%
       layer_multivariate_normal_tri_l(event_size = output_shape)
-    # modelDist <- keras_model_sequential() %>%
-    #   model() %>%
-    #   layer_multivariate_normal_tri_l(event_size = output_shape)
-    ## -------------------
-    # return(list("model" = model, "modelDist" = modelDist))
+    
     return(model)
     
-  }
-  
-  # cnn-multi-site-gaussian-attention -------------------------------------------------------
-  if (topology == "cnn-multi-site-gaussian-attention") {
-    inputs <- layer_input(shape = input_shape)
-    l1 = layer_conv_2d(inputs,filters = 50, kernel_size = kernel_size, activation = "relu", padding = "valid")
-    l2 = layer_conv_2d(l1, filters = 25, kernel_size = kernel_size, activation = "relu", padding = "valid")
-    l3 = layer_conv_2d(l2, filters = 10, kernel_size = kernel_size, activation = "relu", padding = "valid") 
-    l4 = layer_attention(list(l3,l3))
-    l5 = layer_flatten(l4)
-    l6 = layer_dense(l5, units = neurons[1], activation = "relu") 
-    l7 = layer_dense(l6, units = neurons[2], activation = "relu") 
-    l81 = layer_dense(l7, units = output_shape, activation = "linear") 
-    l82 = layer_dense(l7, units = output_shape, activation = "linear") 
-    outputs <- layer_concatenate(list(l81,l82))  
-    model <- keras_model(inputs = inputs, outputs = outputs) 
-    ## -------------------    
-    return(model) 
   }
 }
